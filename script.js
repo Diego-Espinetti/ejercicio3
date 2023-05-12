bbc9ea02aa7938d4ad5c6687dd47e1c361973ceb
Diego-Espinetti <juan.espinetti@davinci.edu.ar>
2023-05-12 01:22:02 -0300

    creando todas las variables

--- script.js
+++ script.js
@@ -20,15 +20,70 @@ televisores[0] = "Televisor-Samsung";
 televisores["Televisor-Samsung"] = {"Codigo": 1,
                                     "Stock":18,
                                     "Img":"img//",
+                                    "Precio":1400,
                                     "Descripcion": "tv 50 pulgadas",
                                     };
 
+televisores[1] = "Televisor-TopHouse";
+televisores["Televisor-TopHouse"] = {"Codigo": 2,
+                                    "Stock":18,
+                                    "Img":"img//",
+                                    "Precio":1000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[2] = "Televisor-Sony";
+televisores["Televisor-Sony"] = {"Codigo": 3,
+                                    "Stock":28,
+                                    "Img":"img//",
+                                    "Precio":3000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[3] = "Televisor-TLC";
+televisores["Televisor-TLC"] = {"Codigo": 4,
+                                    "Stock":48,
+                                    "Img":"img//",
+                                    "Precio":3200,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[4] = "Televisor-Asus";
+televisores["Televisor-Sony"] = {"Codigo": 5,
+                                    "Stock":28,
+                                    "Img":"img//",
+                                    "Precio":3000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[2] = "Televisor-Hitachi";
+televisores["Televisor-Sony"] = {"Codigo": 6,
+                                    "Stock":28,
+                                    "Img":"img//",
+                                    "Precio":3000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[2] = "Televisor-Motorola";
+televisores["Televisor-Sony"] = {"Codigo": 7,
+                                    "Stock":28,
+                                    "Img":"img//",
+                                    "Precio":3000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[2] = "Televisor-LG";
+televisores["Televisor-Sony"] = {"Codigo": 8,
+                                    "Stock":28,
+                                    "Img":"img//",
+                                    "Precio":3000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+televisores[2] = "Televisor-Noblex";
+televisores["Televisor-Sony"] = {"Codigo": 9,
+                                    "Stock":28,
+                                    "Img":"img//",
+                                    "Precio":3000,
+                                    "Descripcion": "tv 50 pulgadas",
+};
+
+
 console.log(televisores)
-console.log(televisores[0])
-
-
-
-
+console.log(televisores["Televisor-Sony"].Precio)
 function VerTelevisores(){
 
 
