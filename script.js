3cef0ae8a6307e19ef9d1a00844fdab48551a6ce
Diego-Espinetti <juan.espinetti@davinci.edu.ar>
2023-05-12 18:34:21 -0300

    empezanado con la funcion

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var televisores = []
 televisores[0] = "Televisor-Samsung";
 televisores["Televisor-Samsung"] = {"Codigo": 1,
                                     "Stock":18,
-                                    "Img":"img//",
+                                    "Img":"img1/descarga(2).jpg",
                                     "Precio":12400,
                                     "Descripcion": "tv 50 pulgadas",
                                     };
@@ -34,49 +34,49 @@ televisores["Televisor-TopHouse"] = {"Codigo": 2,
 televisores[2] = "Televisor-Sony";
 televisores["Televisor-Sony"] = {"Codigo": 3,
                                     "Stock":68,
-                                    "Img":"img//",
+                                    "Img":"img1/descarga(3).jpg",
                                     "Precio":3000,
                                     "Descripcion": "tv 50 pulgadas",
 };
 televisores[3] = "Televisor-TLC";
 televisores["Televisor-TLC"] = {"Codigo": 4,
                                     "Stock":48,
-                                    "Img":"img//",
+                                    "Img":"img1/descarga(4).jpg",
                                     "Precio":3200,
                                     "Descripcion": "tv 50 pulgadas",
 };
 televisores[4] = "Televisor-Asus";
 televisores["Televisor-Asus"] = {"Codigo": 5,
                                     "Stock":85,
-                                    "Img":"img//",
+                                    "Img":"img1/descarga(5).jpg",
                                     "Precio":3000,
                                     "Descripcion": "tv 50 pulgadas",
 };
 televisores[5] = "Televisor-Hitachi";
 televisores["Televisor-Hitachi"] = {"Codigo": 6,
                                     "Stock":28,
-                                    "Img":"img//",
+                                    "Img":"img1/images (1).jpg",
                                     "Precio":3000,
                                     "Descripcion": "tv 50 pulgadas",
 };
 televisores[6] = "Televisor-Motorola";
 televisores["Televisor-Motorola"] = {"Codigo": 7,
                                     "Stock":128,
-                                    "Img":"img//",
+                                    "Img":"img1/images (1).jpg",
                                     "Precio":7000,
                                     "Descripcion": "tv 50 pulgadas",
 };
 televisores[7] = "Televisor-LG";
 televisores["Televisor-LG"] = {"Codigo": 8,
                                     "Stock":348,
-                                    "Img":"img//",
+                                    "Img":"img1/images.jpg",
                                     "Precio":3140,
                                     "Descripcion": "tv 50 pulgadas",
 };
 televisores[8] = "Televisor-Noblex";
 televisores["Televisor-Noblex"] = {"Codigo": 9,
                                     "Stock":258,
-                                    "Img":"img//",
+                                    "Img":"img1/descarga.jpg",
                                     "Precio":1110,
                                     "Descripcion": "tv 50 pulgadas",
 };
@@ -130,15 +130,26 @@ function VerTelevisores(){
 var arraynuevo = [];
 function Ordenanos(){
 
+console.log("Hola desde Ordenans")
 
-    if(televisores["Televisor-Samsung"].Precio> televisores["Televisor-TopHouse"].Precio  || televisores[nombreTv].Precio> televisores["Televisor-Sony"].Precio /* && televisores[nombreTv].Precio> televisores["Televisor-TLC"].Precio && televisores[nombreTv].Precio> televisores["Televisor-Asus"].Precio && televisores[nombreTv].Precio> televisores["Televisor-Hitachi"].Precio && televisores[nombreTv].Precio> televisores["Televisor-Motorola"].Precio   && televisores[nombreTv].Precio> televisores["Televisor-LG"].Precio && televisores[nombreTv].Precio> televisores["Televisor-Noblex"].Precio */){
+    for(i = 0; i<9;i++){
+
+    if(televisores["Televisor-Samsung"].Precio> televisores["Televisor-TopHouse"].Precio  || televisores["Televisor-Samsung"].Precio> televisores["Televisor-Sony"].Precio  || televisores["Televisor-Samsung"].Precio> televisores["Televisor-TLC"].Precio  || televisores["Televisor-Samsung"].Precio> televisores["Televisor-Asus"].Precio  || televisores["Televisor-Samsung"].Precio> televisores["Televisor-Hitachi"].Precio || televisores["Televisor-Samsung"].Precio> televisores["Televisor-Motorola"].Precio   || televisores["Televisor-Samsung"].Precio> televisores["Televisor-LG"].Precio || televisores["Televisor-Samsung"].Precio> televisores["Televisor-Noblex"].Precio ){
+
         precios.push(televisores["Televisor-Samsung"])
+        console.log(precios)
+
     }
     
+     else if( televisores["Televisor-TopHouse"].Precio> televisores["Televisor-Sony"].Precio  || televisores["Televisor-TopHouse"].Precio> televisores["Televisor-TLC"].Precio  || televisores["Televisor-TopHouse"].Precio> televisores["Televisor-Asus"].Precio  || televisores["Televisor-TopHouse"].Precio> televisores["Televisor-Hitachi"].Precio || televisores["Televisor-TopHouse"].Precio> televisores["Televisor-Motorola"].Precio   || televisores["Televisor-TopHouse"].Precio> televisores["Televisor-LG"].Precio || televisores["Televisor-TopHouse"].Precio> televisores["Televisor-Noblex"].Precio ){
+        precios.push(televisores["Televisor-TopHouse"])
+    
     console.log(precios)
-
+ 
 };
 
+}
 
 
 
+}
